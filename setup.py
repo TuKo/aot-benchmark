88dac9392fa3e38c649bb6ea80b083098d48cd18
Javier Turek <javier.turek@intel.com>
2023-11-28 11:20:11 -0800

    Included setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Setuptools setup script."""
+from setuptools import find_packages, setup
+
+setup(
+    name="aot",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=[
+        "torch>=2.1",
+        "matplotlib",
+        'opencv-python',
+        'pillow',
+        'spatial_correlation_sampler',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+        ],
+    },
+    use_scm_version=True,
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    python_requires='>=3.11',
+)
